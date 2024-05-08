ab815f6fb49dab6c7def56d8ad38ab0103d0ec82
hanwool <na990312@naver.com>
2024-05-08 12:50:09 +0900

    auth setting

--- posts/views.py
+++ posts/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def ask(request):
+    #요청으로부터 사용자 정보를 가져온다.
+    user = request.user
+    
+    #가져온 사용자가 '로그인 했는지' 여부를 가져온다.
+    is_authenticated = user.is_authenticated
+    
+    print('user:',user)
+    print('is_authenticated:',is_authenticated)
     return render(request,'posts/ask.html')
