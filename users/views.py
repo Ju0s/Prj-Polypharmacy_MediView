91bb4e111851225407b9bb31e6f5fb11a5d1f1bd
hanwool <na990312@naver.com>
2024-05-08 15:56:48 +0900

    signup page

--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from users.forms import LoginForm, SignupForm
+from users.models import User
 # Create your views here.
 def login_view(request):
     #이미 로그인 되어 있다면
@@ -44,6 +45,32 @@ def logout_view(request):
     return redirect('users/login/')
 
 def signup(request):
-    form = SignupForm()
-    context = {'form':form}
-    return render(request, 'users/signup.html',context)
+    if request.method == 'POST':
+        form = SignupForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password1 = form.cleaned_data['password1']
+            password2 = form.cleaned_data['password2']
+            #입력한 비밀번호 1과 2가 같은지 검사
+            if password1 != password2:
+                form.add_error('password2', '입력된 비밀번호가 서로 다릅니다.')
+            #username을 사용 중인 User가 있는지 검사
+            if User.objects.filter(username=username).exists():
+                form.add_error('username', '입력한 사용자명은 이미 사용중입니다.')
+            #에러가 존재한다면, 에러를 포함한 form을 사용해 회원가입 페이지를 다시 렌더링
+            if form.errors:
+                context = {'form':form}
+                return render(request, 'users/signup.html',context)
+            #에러가 없다면 사용자를 생성하고 로그인 처리 후 ask 페이지로 이동
+            else:
+                user = User.objects.create_user(
+                    username=username,
+                    password=password1,
+                )
+                login(request,user)
+                return redirect('/posts/ask/')
+                
+    else:        
+        form = SignupForm()
+        context = {'form':form}
+        return render(request, 'users/signup.html',context)
