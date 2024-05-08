662330139660a574c92ea7ac90055b8592d47ff4
hanwool <na990312@naver.com>
2024-05-08 13:40:04 +0900

    auth test

--- users/views.py
+++ users/views.py
@@ -1,16 +1,15 @@
 from django.shortcuts import render, redirect
 from users.forms import LoginForm
-
 # Create your views here.
 def login_view(request):
     #이미 로그인 되어 있다면
     if request.user.is_authenticated:
         return redirect('/posts/ask/')
-    return render(request,'users/login.html')
+    
 
     #로그인 폼 인스턴스 생성
     form = LoginForm()
     context = {
         'form' : form,
     }
-    return render(request, 'users/login.html',context)
+    return render(request,'users/login.html',context)
