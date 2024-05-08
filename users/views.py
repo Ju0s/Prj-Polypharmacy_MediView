d913333bf5a299be33be6d840f5a4f9c5e60d464
hanwool <na990312@naver.com>
2024-05-08 13:33:34 +0900

    login page

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from users.forms import LoginForm
 
 # Create your views here.
 def login_view(request):
@@ -6,3 +7,10 @@ def login_view(request):
     if request.user.is_authenticated:
         return redirect('/posts/ask/')
     return render(request,'users/login.html')
+
+    #로그인 폼 인스턴스 생성
+    form = LoginForm()
+    context = {
+        'form' : form,
+    }
+    return render(request, 'users/login.html',context)
