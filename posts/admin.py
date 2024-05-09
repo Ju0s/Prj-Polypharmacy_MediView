548cdf5ccab76ad12fd2208a9e582ebedc441ae8
hanwool <na990312@naver.com>
2024-05-09 14:29:15 +0900

    ask page

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from posts.models import Post, Comment
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'content',
+    ]
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'post',
+        'title',
+        'content',
+    ]
