a2f3396bcf30ff38075199c15c88f7eb38c67051
hanwool <na990312@naver.com>
2024-05-10 17:12:35 +0900

    ask page

--- posts/urls.py
+++ posts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from posts.views import ask, post_add
 
 urlpatterns = [
-    path('ask/',ask),
-    path('post_add/',post_add),
+    path('ask/',ask,name='ask'),
+    path('post_add/',post_add,name='post_add'),
 ]
